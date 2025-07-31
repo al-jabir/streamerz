08a3048635c391de65a91822b944cad914ffdc69
al-jabir <aljabir098@gmail.com>
2025-08-01 00:28:30 +0600

    Add Hero component and integrate it into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <main className="text-sm text-neutral-300 antialiased">
       <Navbar />
+      <Hero />
     </main>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,57 @@
+import { BRAND_LOGOS, HERO_CONTENT } from '../constants';
+import heroImg from '../assets/hero.jpg';
+
+const Hero = () => {
+  return (
+    <section className="pt-28 lg:pt-36">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col items-center text-center">
+        <div className="mb-8 border-neutral-800 px-3 py-2 rounded-full text-xs">{HERO_CONTENT.badgeText}</div>
+
+        <h1 className="text-5xl lg:text-8xl my-4 font-semibold tracking-tighter bg-gradient-to-b from-neutral-50 via-neutral-300 to-neutral-700 bg-clip-text text-transparent">
+          {HERO_CONTENT.mainHeading.split('\n').map((text, idx) => (
+            <span key={idx}>
+              {text}
+              <br />
+            </span>
+          ))}
+        </h1>
+
+        <p className="mt-6 max-w-7xl text-neutral-400">{HERO_CONTENT.subHeading}</p>
+
+        <div className="mt-6 space-x-4">
+          <a
+            href="#"
+            className="inline-block bg-blue-600 hover:bg-blue-500 py-3 px-6 rounded-lg text-white font-medium"
+          >
+            {HERO_CONTENT.callToAction.primary}
+          </a>
+          <a
+            href="#"
+            className=" border border-gray-500 hover:border-gray-400 py-3 px-6 rounded-lg text-white font-medium"
+          >
+            {HERO_CONTENT.callToAction.secondary}
+          </a>
+        </div>
+
+        <div className="py-10">
+          <p className="text-gray-400 text-center mb-8">{HERO_CONTENT.trustedByText}</p>
+          <div className=" flex flex-wrap justify-center gap-8">
+            {BRAND_LOGOS.map((logo, index) => (
+              <img key={index} src={logo.src} alt={logo.alt} className="h-8" />
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-12">
+          <img
+            src={heroImg}
+            alt="stramerz sass interface"
+            className=" w-full h-auto rounded-3xl border border-neutral-800 mb-20"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
